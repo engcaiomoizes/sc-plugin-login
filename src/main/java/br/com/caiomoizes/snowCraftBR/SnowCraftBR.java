240ed6e73d03f339cbab1a177a846e2079630456
Caio M. Santos <engcaiomoizes@gmail.com>
2026-03-03 08:13:46 -0300

    Fix registerCommand function

--- src/main/java/br/com/caiomoizes/snowCraftBR/SnowCraftBR.java
+++ src/main/java/br/com/caiomoizes/snowCraftBR/SnowCraftBR.java
@@ -5,9 +5,15 @@ import br.com.caiomoizes.snowCraftBR.commands.Register;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import org.bukkit.Bukkit;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandMap;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.defaults.BukkitCommand;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
+import java.lang.reflect.Field;
 import java.util.List;
 
 public final class SnowCraftBR extends JavaPlugin {
@@ -63,5 +69,30 @@ public final class SnowCraftBR extends JavaPlugin {
     @Override
     public void onDisable() {
         Bukkit.getServer().getConsoleSender().sendMessage(Component.text("[SnowCraft] Desativado!", NamedTextColor.RED));
+    }
+
+    public void registerCommand(String name, String description, List<String> aliases, String permission, String usage, CommandExecutor executor) {
+        try {
+            Field commandMapField = Bukkit.getServer().getClass().getDeclaredField("commandMap");
+            commandMapField.setAccessible(true);
+
+            CommandMap commandMap = (CommandMap) commandMapField.get(Bukkit.getServer());
+
+            BukkitCommand command = new BukkitCommand(name) {
+                @Override
+                public boolean execute(@NotNull CommandSender sender, @NotNull String label, @NotNull String @NotNull [] args) {
+                    return executor.onCommand(sender, this, label, args);
+                }
+            };
+
+            command.setDescription(description);
+            command.setAliases(aliases);
+            command.setPermission(permission);
+            command.setUsage(usage);
+
+            commandMap.register("snowcraft", command);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
